0798ab299c7085290827d4a6236aff248eeeb487
thiagokf <kfuscothiago@gmail.com>
2024-09-03 11:28:16 -0300

    dasd
    dsa

---
+++ 1 fase/estudos.py
@@ -0,0 +1,142 @@
+#EXERCICIO 1
+
+#funcionario = int(input())
+#num_horas = int(input())
+#hora = float(input())
+
+#salario = num_horas * hora
+
+#print(f"NUMBER = U${funcionario}")
+#print(f"SALARY = U${salario:.2f}")
+
+
+
+#exercicio idade
+
+#total_dias = int(input())
+#anos = total_dias // 365
+#meses = (total_dias % 365) // 30
+#dias = (total_dias % 365) % 30
+
+#print(f"{anos} ano (s)")
+#print(f"{meses} mes (es)")
+#print(f"{dias} dia (s)")
+
+
+
+#exercicio areas
+
+#(a, b, c) = map(float, input().split(' '))
+
+
+#triangulo = (a * c) / 2
+#circulo = (3.14159 * c * c)
+#trapezio = ((a + b) * c) / 2
+#quadrado = b * b
+#retangulo = a * b
+
+#print(f"TRIANGULO: {triangulo:.3f}")
+#print(f"CIRCULO: {circulo:.3f}")
+#print(f"TRAPEZIO: {trapezio:.3F}")
+#print(f"QUADRADO: {quadrado:.3f}")
+#print(f"TRIANGULO: {retangulo:.3f}")
+
+
+
+#exercicio distancia entre dois pontos
+
+#(x1, y1) = map(float, input().split(' '))
+#(x2, y2) = map(float, input().split(' '))
+
+#distancia = ((x2 - x1)*2 + (y2 - y1)2)*0.5
+
+#print(f"{distancia:.4f}")
+
+
+
+#exercicio tempo
+
+#tempo = int(input())
+
+#horas = tempo // 3600
+#resto = tempo % 3600
+#minutos = resto // 60
+#segundo = resto % 60 
+
+
+#print(f"{horas}:{minutos}:{segundo}")
+
+
+
+#exercicio 1017
+
+#h = int(input())
+#km_h = int(input())
+
+#litros = (h * km_h) / 12
+
+#print(f"{litros:.3f}")
+
+
+
+#exercicio 1014
+
+#km = int(input())
+#gas = float(input())
+
+#consumo = km / gas
+
+#print(f"{consumo:.3f} km/l")
+
+
+
+#exercicio 1009
+
+#name = input()
+#salario = float(input())
+#vendas = float(input())
+
+#salario_bonus = salario + (vendas * 0.15)
+
+#print(f"TOTAL = R${salario_bonus:.2f}")
+
+
+
+#exercicio 1016
+
+#km = int(input())
+#distancia = km * 2
+
+#print(f"{distancia} minutos")
+
+
+
+#exercicio 1018
+
+#total_dinheiro = int(input())
+
+#nota_100 = total_dinheiro // 100
+#resto = total_dinheiro % 100
+#nota_50 = resto // 50
+#resto2 = resto % 50
+#nota_20 = resto2 // 20
+#resto3 = resto2 % 20
+#nota_10 = resto3 // 10
+#resto4 = resto3 % 10
+#nota_5 = resto4 // 5
+#resto5 = resto4 % 5
+#nota_2 = resto5 // 2
+#nota_1 = resto5 % 2
+
+#print(total_dinheiro)
+#print(f"{nota_100} nota(s) de R$ 100,00")
+#print(f"{nota_50} nota(s) de R$ 50,00" )
+#print(f"{nota_20} nota(s) de R$ 20,00")
+#print(f"{nota_10} nota(s) de R$ 10,00")
+#print(f"{nota_5} nota(s) de R$ 5,00")
+#print(f"{nota_2} nota(s) de R$ 2,00")
+#print(f"{nota_1} nota(s) de R$ 1,00")
+
+
+
+
