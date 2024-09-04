29ae33d14a5aa6a69c771f1d128a04b4427433fa
thiagokf <kfuscothiago@gmail.com>
2024-09-04 13:39:48 -0300

    Update estudos.py

--- 1 fase/estudos.py
+++ 1 fase/estudos.py
@@ -302,10 +302,39 @@
 # elif salario > 4500:
 #     imposto = (salario - 4500) * 0.28 + (1500 * 0.18) + (1000 * 0.08)
 #     print(f"R$ {imposto:.2f}")
-    
-    
 
 
 
 
+#tipos de triangulos
+#Leia 3 valores de ponto flutuante A, B e C e ordene-os em ordem decrescente, de modo que o lado A representa o maior dos 3 lados. A seguir, determine o tipo de triângulo que estes três lados formam, com base nos seguintes casos, sempre escrevendo uma mensagem adequada:
+
+(x, y, z) = map(float, input().split(' '))
+
+if (x >= y) and (x >= z):
+    a = x
+    b = y
+    c = z
+if (y >= x) and (y >= z):
+    a = y
+    b = x 
+    c = z
+if (z >= x) and (z >= z):
+    a = z
+    b = x
+    c = y
+
+
+if a >= b + c:
+    print("NAO FORMA TRIANGULO")
+elif (a**2 == b**2 + c**2):
+    print("TRIANGULO RETANGULO")
+elif (a**2 > b**2 + c**2):
+    print("TRIANGULO OBTUSANGULO")
+elif (a**2 < b**2 + c**2):
+    print("TRIANGULO ACUTANGULO")
+if a == b == c:
+    print("TRIANGULO EQUILATERO")
+elif a == b or b == c or a == c:
+    print("TRIANGULO ISOSCELES")
 
