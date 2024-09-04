df284c2583a33d3cd370eaf7d1b0f08b0358749e
thiagokf <kfuscothiago@gmail.com>
2024-09-04 12:17:57 -0300

    Update estudos.py

--- 1 fase/estudos.py
+++ 1 fase/estudos.py
@@ -288,19 +288,20 @@
 
 # exercicio 1051
 
-salario = float(input())
-if salario <= 2000:
-    imposto = 0
-    print("isento")
-elif salario <= 3000:
-    imposto = (salario - 2000) * 0.08
-    print(f"R$ {imposto:.2f}")
-elif salario <= 4500:
-    imposto = (salario - 3000) * 0.18 + (1000 * 0.08)
-    print(f"R$ {imposto:.2f}")
-elif salario > 4500:
-    imposto = (salario - 4500) * 0.28 + (1500 * 0.18) + (1000 * 0.08)
-    print(f"R$ {imposto:.2f}")
+# salario = float(input())
+# 
+# if salario <= 2000:
+#     imposto = 0
+#     print("Isento")
+# elif salario <= 3000:
+#     imposto = (salario - 2000) * 0.08
+#     print(f"R$ {imposto:.2f}")
+# elif salario <= 4500:
+#     imposto = (salario - 3000) * 0.18 + (1000 * 0.08)
+#     print(f"R$ {imposto:.2f}")
+# elif salario > 4500:
+#     imposto = (salario - 4500) * 0.28 + (1500 * 0.18) + (1000 * 0.08)
+#     print(f"R$ {imposto:.2f}")
     
     
 
