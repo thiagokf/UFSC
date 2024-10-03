a27eb20da119b27f304985d0f5e5e50de1e37055
thiagokf <kfuscothiago@gmail.com>
2024-10-03 14:30:55 -0300

    Update semana 6.py

--- 1 fase/semana 6.py
+++ 1 fase/semana 6.py
@@ -1,60 +1,60 @@
-# # binario --> decimal
-# binario = input()
-# total = len(binario)
-# 
-# lista = []
-# 
-# for i in range(total):
-#     lista.append(binario[i])
-#     
-# decimal = 0
-# n = 1
-# for i in range(total):
-#     lista_num = int(lista[i])
-#     decimal = decimal + (lista_num * (2** (total - n)))
-#     i += 1
-#     n += 1
-# print(decimal)
-# 
-# 
-# # decimal --> binario
-# decimal = int(input())
-# 
-# binario = []
-# 
-# while decimal > 0:
-#     d = decimal % 2
-#     decimal = decimal // 2
-#     binario.append(d)
-# t = len(binario)
-# while t != 0:
-#     print(binario[t-1], end='')
-#     t -= 1
-# 
-# 
-# # Lista de Exercicio Tuplas e Listas:
-# 
-# # Inicialização  
-# lista = []
-# media = 0
-# melhor_nota = 0
-# menor_nota = 11
-# 
-# # Notas
-# for i in range(3):
-#     nota = float(input())
-#     lista.append(nota)
-#     media += nota
-# 
-# # Melhor e pior nota
-# melhor_nota = max(lista)
-# menor_nota = min(lista)
-# 
-# # Resultados
-# print(f"media = {media / 3:.2f}")   
-# print(f"melhor nota = {melhor_nota}")
-# print(f"menor nota = {menor_nota}")
-# print(f"diferença entre a maior e a menor nota = {melhor_nota - menor_nota}")
+# binario --> decimal
+binario = input()
+total = len(binario)
+
+lista = []
+
+for i in range(total):
+    lista.append(binario[i])
+    
+decimal = 0
+n = 1
+for i in range(total):
+    lista_num = int(lista[i])
+    decimal = decimal + (lista_num * (2** (total - n)))
+    i += 1
+    n += 1
+print(decimal)
+
+
+# decimal --> binario
+decimal = int(input())
+
+binario = []
+
+while decimal > 0:
+    d = decimal % 2
+    decimal = decimal // 2
+    binario.append(d)
+t = len(binario)
+while t != 0:
+    print(binario[t-1], end='')
+    t -= 1
+
+
+# Lista de Exercicio Tuplas e Listas:
+
+# Inicialização  
+lista = []
+media = 0
+melhor_nota = 0
+menor_nota = 11
+
+# Notas
+for i in range(3):
+    nota = float(input())
+    lista.append(nota)
+    media += nota
+
+# Melhor e pior nota
+melhor_nota = max(lista)
+menor_nota = min(lista)
+
+# Resultados
+print(f"media = {media / 3:.2f}")   
+print(f"melhor nota = {melhor_nota}")
+print(f"menor nota = {menor_nota}")
+print(f"diferença entre a maior e a menor nota = {melhor_nota - menor_nota}")
 
 
 
@@ -76,3 +76,42 @@ if len(lista) != len(set(lista)): # set() elimina duplicatas na lista
 else:
     print("não há valores repetidos na lista")
     
+
+# # exercicio 3 # #
+
+tupla = (5, 12, 3, 7, 9, 21, 1, 8, 15, 4)
+
+# pegando o maximo e o minimo
+maximo = max(tupla)
+minimo = min(tupla)
+
+# posicoes do max e min
+pos_max = tupla.index(maximo)
+pos_min = tupla.index(minimo)
+
+print(f"O maior numero é o {maximo} e esta na posição {pos_max}")
+print(f"O menor numero é o {minimo} e esta na posição {pos_min}")
+
+
+# # # exercicio 4 # # #
+
+# pedindo os numeros e lista
+n = int(input("digite quantos numeros tera na lista: "))
+x = []
+z = int(input("digite o numero para multiplicar os elementos da lista: "))
+
+# listando os numeros
+for i in range(n):
+    numero = int(input("numero: "))
+    x.append(numero)
+print(x )
+# multiplicacao
+print(f'multiplicado por {z}: ')
+p = 0
+m = []
+for i in range(n):
+    p = x[i] * z
+    m.append(p)
+print(m)
+    
+
