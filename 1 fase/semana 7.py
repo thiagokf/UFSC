d9362bc3f60e2dc327f662eee99df4abb666368f
thiagokf <kfuscothiago@gmail.com>
2024-10-07 17:05:41 -0300

    Create semana 7.py

---
+++ 1 fase/semana 7.py
@@ -0,0 +1,23 @@
+# # # exercicio 1 # # #
+lista = []
+lista_par = []
+for i in range(5):
+    n = int(input("digite um numer para a lista: "))
+    lista.append(n)
+    if n % 2 == 0:
+        lista_par.append(n)
+    
+    
+qnt9 = lista.count(9)
+if 3 in lista:
+    onde3 = lista.index(3)
+else:
+    onde3 = None
+
+print(f"o numero 9 aparece {qnt9} vezes na lista")
+if onde3:
+    print(f"o numero 3 aparece na posição {onde3} da lista")
+else:
+    print(f"o numero 3 nao aparece na lista")
+print(f"a lista contem os numero pares: {lista_par}")
+
