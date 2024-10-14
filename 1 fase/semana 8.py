7d3d65e1d2e0c5c7deefbfad9b771af0bc45be72
thiagokf <kfuscothiago@gmail.com>
2024-10-14 16:49:26 -0300

    Update semana 8.py

--- 1 fase/semana 8.py
+++ 1 fase/semana 8.py
@@ -42,3 +42,46 @@ for pessoa in maior20:
     print(f"- {pessoa[0]} (Idade: {pessoa[1]} anos)")
 
 
+ # # exercicio 2 # #
+tamanho = int(input("tamanho da matriz: "))
+l = int(input())
+c = input().upper()
+
+op = 0
+matriz = []
+for i in range(tamanho):
+    linha = []
+    for j in range(tamanho):
+        linha.append(float(input()))
+    matriz.append(linha)
+print(matriz)
+
+
+for i in range(tamanho):
+    op += matriz[l][i]
+if c == 'M':
+    op = op / tamanho
+print(op)
+
+
+ # # exercicio 3 # #
+tamanho = int(input("tamanho da matriz: "))
+a = input().upper()
+op = 0
+matriz = []
+for l in range(tamanho):
+    linha = []
+    for c in range(tamanho):
+        linha.append(float(input()))
+    matriz.append(linha)
+for linha in matriz:
+    print(linha)
+
+for i in range(tamanho):
+    for j in range(i):  
+        op += matriz[i][j]
+        
+if a == 'M':
+    op = op / tamanho
+print(op)
+
