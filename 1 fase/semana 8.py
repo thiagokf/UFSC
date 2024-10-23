86cbcf3cb683fe684bb37738d4700d0c52cc1c67
Thiago Fusco <165811535+thiagokf@users.noreply.github.com>
2024-10-23 20:04:46 -0300

    Update semana 8.py

--- 1 fase/semana 8.py
+++ 1 fase/semana 8.py
@@ -341,3 +341,39 @@ for c in range(n):
         
     elif c == n -1:
         print("esse aluno não foi cadastrado")
+
+
+# # # semana 9 # # #
+
+matriculados = []
+
+um_esporte = []
+dois_esporte = []
+
+esporte = []
+
+while True:
+    aluno = input("nome do aluno: ")
+    if aluno == '0':
+        break
+    esporte1 = input("primeiro esporte: ").lower()
+    esporte2 = input("segundo esporte: ").lower()
+    
+    if esporte2 == 'no':
+        um_esporte.append(aluno)
+        esporte.append(esporte1)
+    else:
+        dois_esporte.append(aluno)
+        esporte.append(esporte1)
+        esporte.append(esporte2)
+        
+
+matriculados.append([um_esporte, dois_esporte, esporte])
+fut = esporte.count('futebol')
+basca = esporte.count('basquete')
+natacao = esporte.count('natacao')
+volei = esporte.count('volei')
+
+print(f"alunos matriculados: {len(um_esporte) + len(dois_esporte)}")
+print(f"os alunos que possuem o direito do desconto são {dois_esporte}")
+print(f"numero de alunos por modalidade: {fut} em futebol, {basca} em basquete, {natacao} em natacao e {volei} em volei")
