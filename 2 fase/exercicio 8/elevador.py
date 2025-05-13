0d36c271af7afbd5f98571dc397cb6002af550d2
Thiago Fusco <165811535+thiagokf@users.noreply.github.com>
2025-05-12 21:14:09 -0300

    Arrumar os metodos do jeito do tiagao

---
+++ 2 fase/exercicio 8/elevador.py
@@ -0,0 +1,71 @@
+from abstractElevador import AbstractElevador
+from elevadorCheioException import ElevadorCheioException
+from elevadorJahNoTerreoException import ElevadorJahNoTerreoException
+from elevadorJahNoUltimoAndarException import ElevadorJahNoUltimoAndarException
+from elevadorJahVazioException import ElevadorJahVazioException
+
+
+class Elevador(AbstractElevador): 
+    def __init__(self, andar_atual: int, total_andares_predio: int,
+                 pessoas_no_elevador: int, total_pessoas: int):
+        self.__andar_atual = andar_atual
+        self.__total_andares_predio = total_andares_predio
+        self.__pessoas_no_elevador = pessoas_no_elevador
+        self.__total_pessoas = total_pessoas
+
+    # elevadorJahNoTerreo    
+    def descer(self):
+        try:
+            aux = self.__andar_atual - 1
+            if aux < 0:
+                self.__andar_atual = aux
+
+        except ElevadorJahNoTerreoException as e:
+            print(e)
+
+    # elevadorCheio    
+    def entra_pessoa(self):
+        try:
+            aux = self.__pessoas_no_elevador + 1
+            if aux < self.__capacidade:
+                self.__pessoas_no_elevador = aux
+        
+        except ElevadorCheioException as e:
+            print(e)
+
+    # elevadorVazio
+    def sai_pessoa(self) -> str:
+        if self.__pessoas_no_elevador - 1 < 0:
+            raise ElevadorJahVazioException()
+        
+        self.__pessoas_no_elevador - 1
+    
+    # elevador ja no maximo
+    def subir(self) -> str:
+        if self.__andar_atual + 1 > self.__total_andares_predio:
+            raise ElevadorJahNoUltimoAndarException()
+
+        self.__andar_atual += 1
+    @property
+    def capacidade(self) -> int:
+        if isinstance(self.__capacidade, int)
+        return self.__capacidade
+    
+    @property
+    def total_pessoas(self) -> int:
+        if isinstance(self.__total_pessoas, int)
+        return self.__total_pessoas
+    
+    @property
+    def total_andares_predio(self) -> int:
+        if isinstance(self.__total_andares_predio, int)
+        return self.__total_andares_predio
+    
+    @property
+    def andar_atual(self) -> int:
+        if isinstance(self.__andar_atual, int)
+        return self.__andar_atual
+    
+    @total_andares_predio.setter
+    def total_andares_predio(self, total_andares_predio: int):
+        self.__total_andares_predio = total_andares_predio
