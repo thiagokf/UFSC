ed9999c6a54ac871d4dff1041e3aa06e48f45616
Thiago Fusco <165811535+thiagokf@users.noreply.github.com>
2025-05-13 19:36:54 -0300

    Update controladorElevador.py

--- 2 fase/exercicio 8/controladorElevador.py
+++ 2 fase/exercicio 8/controladorElevador.py
@@ -4,10 +4,10 @@ from comandoInvalidoException import ComandoInvalidoException
 
 class ControladorElevador(AbstractControladorElevador):
 	def __init__(self, elevador: Elevador):
-	self.__elevador = elevador
+		self.__elevador = elevador
 	
 	def inicializar_elevador(self, andar_atual: int, total_andares_predio: int, capacidade: int, total_pessoas: int):
-	elevador = Elevador(andar_atual, total_andares_predio, capacidade, total_pessoas)
+		elevador = Elevador(andar_atual, total_andares_predio, capacidade, total_pessoas)
 	
 	def subir(self):
 		return self.__elevador.subir()
@@ -17,7 +17,7 @@ class ControladorElevador(AbstractControladorElevador):
 		
 	def entra_pessoa(self) -> str:
 		return self.__elevador.entra_pessoa()
-
+	
 	def sai_pessoa() -> str:
 		return self.__elevador.sai_pessoa()
 		
