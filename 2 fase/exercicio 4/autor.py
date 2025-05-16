57bc9dd41b62234df2c315dd9654497dc9520e96
Thiago Fusco <165811535+thiagokf@users.noreply.github.com>
2025-05-16 11:06:45 -0300

    Create autor.py

---
+++ 2 fase/exercicio 4/autor.py
@@ -0,0 +1,23 @@
+class Autor:
+    def __init__(self, codigo, nome):
+        self.__codigo = codigo
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome):
+        if isinstance(nome, str):
+            self.__nome = nome
+            
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    
+    @codigo.setter
+    def codigo(self, codigo):
+        if isinstance(codigo, int):
+            self.__codigo = codigo
