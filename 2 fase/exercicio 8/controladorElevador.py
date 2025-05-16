6b4fe397b88ff0c230c16d800135e91341886c2f
Thiago Fusco <165811535+thiagokf@users.noreply.github.com>
2025-05-16 11:02:34 -0300

    controladorElevador finalizado

--- 2 fase/exercicio 8/controladorElevador.py
+++ 2 fase/exercicio 8/controladorElevador.py
@@ -1,27 +1,29 @@
 from abstractControladorElevador import AbstractControladorElevador
 from comandoInvalidoException import ComandoInvalidoException
+from elevador import Elevador
 
 
 class ControladorElevador(AbstractControladorElevador):
-	def __init__(self, elevador: Elevador):
-		self.__elevador = elevador
-	
-	def inicializar_elevador(self, andar_atual: int, total_andares_predio: int, capacidade: int, total_pessoas: int):
-		elevador = Elevador(andar_atual, total_andares_predio, capacidade, total_pessoas)
-	
-	def subir(self):
-		return self.__elevador.subir()
-	
-	def descer(self) -> str:
-		return self.__elevador.descer()
-		
-	def entra_pessoa(self) -> str:
-		return self.__elevador.entra_pessoa()
-	
-	def sai_pessoa() -> str:
-		return self.__elevador.sai_pessoa()
-		
-	@property
-	def elevador(self) -> Elevador:
-	if isinstance(elevador, Elevador):
-		return self.__elevador
+    def __init__(self):
+        self.__elevador = None
+
+    def inicializar_elevador(self, andar_atual: int, total_andares_predio: int,
+                             capacidade: int, total_pessoas: int):
+        self.__elevador = Elevador(andar_atual, total_andares_predio,
+                                   capacidade, total_pessoas)
+
+    def subir(self):
+        return self.__elevador.subir()
+
+    def descer(self) -> str:
+        return self.__elevador.descer()
+
+    def entra_pessoa(self) -> str:
+        return self.__elevador.entra_pessoa()
+
+    def sai_pessoa(self) -> str:
+        return self.__elevador.sai_pessoa()
+
+    @property
+    def elevador(self) -> Elevador:
+        return self.__elevador
